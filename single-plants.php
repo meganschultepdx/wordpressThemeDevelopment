9ba5da224ac6d89ebb44f335da2606209085858c
megan schulte <megan.schulte.pdx@gmail.com>
2020-02-07 12:40:02 -0800

    add advanced custom fields

--- single-plants.php
+++ single-plants.php
@@ -5,35 +5,36 @@
 		<div class="d-flex justify-content-center">
             <h1><?php the_title();?></h1>
         </div>
-        <?php if (has_post_thumbnail()):?>
-            
-            <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 mt-4 img-thumbnail">
-            
-            <?php endif;?>
-        </div>
-         <div class="row">
-            <div class="col-lg-6 col-md-4 ml-4 mr-4">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if (has_post_thumbnail()):?>
+                    
+                    <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 mt-4 img-thumbnail">
+                    
+                    <?php endif;?>
                 
                 <?php get_template_part('includes/section', 'blogcontent');?>
                 
                 <?php wp_link_pages();?>
-                </div>
+            </div>
                 
-                    <div class="col-lg-6 col-md-4">
-                        <ul>
-                            <li>Common Name: <?php echo get_post_meta($post->ID, 'Common Name:', true);?></li>
-
-                            <?php if (get_post_meta($post->ID, 'Latin Name:', true)):?>
-
-                            <li>Latin Name: <?php echo get_post_meta($post->ID, 'Latin Name:', true);?></li>
-                            
-                            <?php endif;?>
-
-                            <li>Family: <?php echo get_post_meta($post->ID, 'Family:', true);?></li>
-                            <li>Zones: <?php echo get_post_meta($post->ID, 'Zones:', true);?></li>
-                        </ul>
-                    </div>
-
+            <div class="col-lg-6">
+                <ul>
+                    <li>
+                        Common Name: <?php the_field('common_name:');?>
+                    </li>
+                    <li>
+                        Latin Name: <?php the_field('latin_name:');?>
+                    </li>
+                    <li>
+                        Family: <?php the_field('plant_family:');?>
+                    </li>
+                    <li>
+                        Zones: <?php the_field('zones:');?>
+                    </li>
+                </ul>
+            </div>
+        </div>
 	</div>
 </section>
 
